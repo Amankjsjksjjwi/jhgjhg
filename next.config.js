0dff5f07b3133a07ae8927444d41318cdeab8f46
Amankjsjksjjwi <jk661060@gmail.com>
2024-12-16 00:07:49 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  env: {
+    publicSupabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    publicSupabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    cloudinaryCloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    cloudinaryAPIKey: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    cloudinaryAPISecret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
+
+// async redirects() {
+//   return [
+//     {
+//       source: '/',
+//       destination: '/',
+//       permanent: true,
+//     },
+//   ];
+// },
